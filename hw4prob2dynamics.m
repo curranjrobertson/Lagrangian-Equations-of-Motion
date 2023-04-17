5ab09b228eb52f356bf5ce519c6377074b5bef0e
Curran Robertson <100748551+curranjrobertson@users.noreply.github.com>
2023-04-16 19:11:31 -0600

    semicolons

--- hw4prob2dynamics.m
+++ hw4prob2dynamics.m
@@ -22,8 +22,8 @@ pretty(L)
 d1 = diff(L, dtheta) ;
 d2 = diff(L, theta);
     % DOF 2 : x
-d4 = diff(L, dx)
-d5 = diff(L, x)
+d4 = diff(L, dx);
+d5 = diff(L, x);
 
 % Time Derivatives
     % DOF 1 : theta
@@ -31,14 +31,14 @@ syms dtheta(t)
 d3 = diff(subs(d1, dtheta, dtheta(t)), t);
     % DOF 2 : x
 syms dx(t) theta(t)
-d4_update = dx(t)*m1 + dx(t)*m2 + dx(t)*m3 + m1*r*cos(theta(t))
-d6 = diff(d4_update, t)
+d4_update = dx(t)*m1 + dx(t)*m2 + dx(t)*m3 + m1*r*cos(theta(t));
+d6 = diff(d4_update, t);
 
 % Eqns of Motion
     % DOF 1 : theta
 eqn1 = d3 - d2 == 0;
     % DOF 2 : x
-eqn2 = d6 - d5 == 0
+eqn2 = d6 - d5 == 0;
 
 % Solutions
     % DOF 1 : theta
